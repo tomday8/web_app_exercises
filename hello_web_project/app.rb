2555f0e23ce93864bb261efa8c6f8e5201b46d66
Tom Day <111976454+tomday8@users.noreply.github.com>
2022-10-25 09:20:08 +0100

    First commit.

---
+++ hello_web_project/app.rb
@@ -0,0 +1,58 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+
+#   configure :development do
+#     register Sinatra::Reloader
+#   end
+
+
+  # Declares a route that responds to a request with:
+  #  - a GET method
+  #  - the path /
+
+#   get '/' do
+
+#     # The code here is executed when a request is received,
+#     # and need to send a response. 
+
+#     # We can simply return a string which
+#     # will be used as the response content.
+#     # Unless specified, the response status code
+#     # will be 200 (OK).
+
+#    return 'Some response data'
+#   end
+
+    get '/' do
+        "Hello World"
+    end
+
+
+    get '/hello' do
+        name = params[:name]
+        return "Hello #{name}"
+    end
+
+    post '/submit' do
+        name = params[:name]
+        message = params[:message]
+        return "Thanks #{name}, you sent this message: '#{message}'"
+    end
+
+    get '/names' do 
+        names = params[:names]
+        return names
+    end
+
+    post '/sort-names' do
+        names = params[:names]
+        names_split = names.split(",")
+        names_sorted = names_split.sort
+        return names_sorted.join(",")
+    end
+end
---
+++ hello_web_project/spec/integration/app_spec.rb
@@ -0,0 +1,55 @@
+# file: spec/integration/application_spec.rb
+
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+  context "GET to /" do
+    it "returns 200 OK with the right content" do
+      # Send a GET request to /
+      # and returns a response object we can test.
+      response = get("/")
+
+      # Assert the response status code and body.
+      expect(response.status).to eq(200)
+      expect(response.body).to eq("Hello World")
+    end
+  end
+
+  context "POST to /submit" do
+    it "returns 200 OK with the right content" do
+      # Send a POST request to /submit
+      # with some body parameters
+      # and returns a response object we can test.
+      response = post("/submit", name: "Tom", message: "Hello World!")
+
+      # Assert the response status code and body.
+      expect(response.status).to eq(200)
+      expect(response.body).to eq("Thanks Tom, you sent this message: 'Hello World!'")
+    end
+  end
+
+  context "GET to /names" do
+    it "returns 200 OK with the right content" do
+      response = get("/names", names: "Julia, Mary, Karim")
+      expect(response.status).to eq(200)
+      expect(response.body).to eq("Julia, Mary, Karim")
+    end
+  end
+
+  context "POST to /sort-names" do
+    it 'returns 200 OK and names sorted' do
+      response = post('/sort-names', names: 'Joe,Alice,Zoe,Julia,Kieran')
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('Alice,Joe,Julia,Kieran,Zoe')
+    end
+  end
+end
